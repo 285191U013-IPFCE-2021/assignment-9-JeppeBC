69edfd01d3cf126511acffccfcd5736a3160776a
JeppeBC <202106888@post.au.dk>
2021-11-07 09:03:46 +0100

    assignment week 9

--- src/search.c
+++ src/search.c
@@ -1,7 +1,15 @@
 #include "search.h"
 #include <stdbool.h>
+#include <assert.h>
 
-bool search(int a[], int n, int x)
-{
-    return 0;
+bool search(int a[], int n, int x){
+assert(n>=0); //precondition
+
+//recurtion
+  if(n==0)
+    return false;
+  else if(a[n-1]==x && n>0)
+    return true;
+  else
+    return search(a,n-1,x);
 }
--- src/sum.c
+++ src/sum.c
@@ -1,6 +1,12 @@
 #include "sum.h"
+#include<assert.h>
 
-int sum(int a[], int n)
-{
-    return 0;
+int sum(int a[], int n){
+assert(n>=0); //precondition
+
+//recurtion
+if (n==0)
+  return n;
+else
+  return (a[n-1]+sum(a,n-1));
 }
